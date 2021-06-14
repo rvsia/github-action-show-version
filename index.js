70ec548f833b8226272ce5ea02ef05e6265efbb0
rvsia <r.vsia@seznam.cz>
2021-06-14 16:26:13 +0200

    Add custom commit analyzer

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const nameToGreet = core.getInput('who-to-greet');
+    const commits = github.context.payload.commits;
+
+    let nextVersion = 0;
+
+    commits.forEach((commit) => {
+        if(commit.message.match(/^fix(\w+):/)) {
+            nextVersion = nextVersion === 0 ? 1 : nextVersion;
+        } else if (commit.message.match(/^feat(\w+):/)) {
+            nextVersion = 2;
+        }
+    })
+
+    if(nextVersion === 0) {
+        console.log('no version will be released')
+    } else if(nextVersion === 1) {
+        console.log('next version is fix')
+    } else {
+        console.log('next version is feature')
+    }
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
