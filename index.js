66b1444d47374927d8f5023f309101c9c35fc172
rvsia <r.vsia@seznam.cz>
2021-06-15 13:02:27 +0200

    parse commits correctly

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ const main = async () => {
 
     const commits = await fetch(github.context.payload.pull_request.commits_url).then(data => data.json())
 
-    console.log('commits', commits)
-
     let nextVersion = 0;
 
-    commits.forEach((commit) => {
+    commits.forEach(({commit}) => {
         console.log('analyzing: ', commit.message)
         if (commit.message.match(/^fix\(\w+\):/)) {
             nextVersion = nextVersion === 0 ? 1 : nextVersion;
